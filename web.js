b55e7b4168b94e0a826ef0f397f4a0a9b32ed541
Carey McLean <carey@theappchicks.com>
2013-07-12 06:16:55 +0000

    updating web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var myContent = fs.readFileSync("index.html", "utf8");
+
+
+console.log("reading my index file: " + myContent);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World3!');
 });
 
 var port = process.env.PORT || 5000;
